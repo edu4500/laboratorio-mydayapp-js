b5a2a20b80754145f772ae094f4cd05e1b4a8699
eduardo.sutta <eduardo.sutta>
2023-08-17 00:31:36 -0500

    change storage

--- src/js/storage.js
+++ src/js/storage.js
@@ -1,9 +1,22 @@
 
 const TABLE = "TAREAS";
-export const storage = () => {
+export const Storage = () => {
+
     let getElemntos = () => {
-        let strTareas = localStorage.getItem(TABLE);
-        let tareas = JSON.stringify(strTareas);
-        return tareas
+        // let strTareas = localStorage.getItem(TABLE);
+        // let tareas = JSON.stringify(strTareas);
+        return localStorage.getItem(TABLE);
+    }
+    
+    let memoryTareas = getElemntos();
+    let length = (()=>{
+        debugger
+        if(memoryTareas===undefined || memoryTareas===null) return 0;
+        return memoryTareas.length;
+    })();
+
+    return {
+        memoryTareas,
+        length
     }
 }
