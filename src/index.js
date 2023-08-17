842ae672ad0fa52bae36b07bc82d5e0bd8fa3579
eduardo.sutta <eduardo.sutta>
2023-08-17 01:43:50 -0500

    refactory vistaListTareas

--- src/index.js
+++ src/index.js
@@ -4,20 +4,28 @@ import { main,footer,newTodo } from "./js/names";
 import { Storage } from "./js/storage";
 import { Tarea } from "./js/models";
 
+
+
+
 (function (){
   let _storage = new Storage();
-  if(_storage.length()==0){
-    main.setAttribute("hidden","hidden")
-    footer.setAttribute("hidden","hidden")
+  function visibleListTareas(){
+    if(_storage.length()==0){
+      main.setAttribute("hidden","hidden")
+      footer.setAttribute("hidden","hidden")
+    }else{
+      main.removeAttribute("hidden")
+      footer.removeAttribute("hidden")
+    }
   }
+  visibleListTareas()
   newTodo.addEventListener('keyup',(event)=>{
     if(event.key == 'Enter'){
       let value = newTodo.value.trim();
       _storage.addTarea(new Tarea(value))
       newTodo.value = "";
+      visibleListTareas()
     }
   })
-
 })();
-
 
