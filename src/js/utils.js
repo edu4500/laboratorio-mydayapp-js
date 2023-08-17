f5e252c4569e7ae0914090e9b20295e2a513de15
eduardo.sutta <eduardo.sutta>
2023-08-17 00:32:04 -0500

    add hidden

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,16 @@
 export const sayHello = (text) => {
   return text;
 };
+
+export const Util = () => {
+  let hiddenMain = () => {
+    document.getElementsByClassName("main")[0].setAttribute("hidden","hidden")
+  }
+  let hiddenFooter = () => {
+    document.getElementsByClassName("footer")[0].setAttribute("hidden","hidden")
+  }
+  return {
+    hiddenMain,
+    hiddenFooter
+  }
+}
