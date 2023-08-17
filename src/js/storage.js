7d13a078cd533ed1e7916788a68c1b8868c3785d
eduardo.sutta <eduardo.sutta>
2023-08-16 23:53:36 -0500

    add get Elemntos

--- src/js/storage.js
+++ src/js/storage.js
@@ -1,6 +1,9 @@
 
 const TABLE = "TAREAS";
 export const storage = () => {
-
-
+    let getElemntos = () => {
+        let strTareas = localStorage.getItem(TABLE);
+        let tareas = JSON.stringify(strTareas);
+        return tareas
+    }
 }
