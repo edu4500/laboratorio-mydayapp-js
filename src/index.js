36cf5c4410b65061767833ce4620cc3b02df7c5c
eduardo.sutta <eduardo.sutta>
2023-09-04 20:25:00 -0500

    fin

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import { sayHello } from "./js/utils";
 
 sayHello('hello')
 
-var topID = 0;
+var topID = function(){
+    var date = new Date().getTime();
+    var number = Math.floor(Math.random()*1000)
+    var strIndex = date.toString() + number.toString()
+    return parseInt(strIndex)
+};
 var TodoList = getLocalStorage()
 
+var listFilter = document.getElementsByClassName('filters').item(0)
 
 function getLocalStorage(){
     return JSON.parse(localStorage.getItem('mydayapp-js')) || []
@@ -30,10 +36,7 @@ function showElement(){
 function render(){
     showElement()
     pintarTodoCount()
-    TodoList.forEach(item => {
-        let nodo = createTodoItem(item)
-        document.getElementById('todo-list').appendChild(nodo)
-    })
+    filtrarTasks('')
 }
 
 function createTodoItem(t){
@@ -41,6 +44,27 @@ function createTodoItem(t){
     if(t.completed) nodo.className = 'completed'
     let div = document.createElement("div")
     div.className = 'view'
+    let edit = document.createElement('input')
+    edit.className = 'edit'
+    
+    edit.addEventListener('keyup',(event)=>{
+        if(event.key==='Escape'){
+            nodo.className = ''
+        }
+        else if(event.key === 'Enter'){
+            label.innerText = edit.value.trim()
+            t.title = edit.value.trim()
+            nodo.className = ''
+            guardarLocalStorage()
+        }
+    })
+
+    nodo.appendChild(edit)
+    nodo.addEventListener('dblclick',(event)=>{
+        edit.value = t.title
+        nodo.className = 'editing'
+    })
+    
     nodo.appendChild(div)
   
     let checkbox = document.createElement('input')
@@ -63,28 +87,7 @@ function createTodoItem(t){
     div.appendChild(checkbox)
     let label = document.createElement('label')
     label.innerText = t.title
-    label.addEventListener('click',(event)=>{
-        // <input class="edit" value="Make dishes" />
-        let edit = document.createElement('input')
-        edit.className = 'edit'
-        edit.value = t.title
-        nodo.className = 'editing'
-        nodo.appendChild(edit)
-        edit.addEventListener('keyup',(event)=>{
-            if(event.key==='Escape'){
-                deleteEditing()
-            }
-            else if(event.key === 'Enter'){
-                label.innerText = edit.value.trim()
-                t.title = edit.value.trim()
-                deleteEditing()
-            }
-        })
-        function deleteEditing(){
-            nodo.className = ''
-            edit.remove()
-        }
-    })
+  
     div.appendChild(label);
     let destroy = document.createElement('button')
     destroy.addEventListener('click',(event)=>{
@@ -92,6 +95,7 @@ function createTodoItem(t){
         let index = TodoList.findIndex(item => item.id === t.id)
         TodoList.splice(index,1)
         showElement()
+        guardarLocalStorage()
     })
     destroy.className = 'destroy'
     div.appendChild(destroy)
@@ -100,13 +104,20 @@ function createTodoItem(t){
     return nodo;
 }
 var todoCount = document.getElementById('todo-count')
+var clearCompleted = document.getElementById('clear-completed')
 function pintarTodoCount(){
     var numero = 0
     TodoList.forEach(t => {
         if(t.completed==false) numero++
     })
+    if(numero==TodoList.length){
+        clearCompleted.setAttribute('hidden','hidden')
+    }
+    else{
+        clearCompleted.removeAttribute('hidden')  
+    }
     var plural = numero==1?'':'s'
-    todoCount.innerHTML = `<strong id="count">${numero}</strong> item left${plural}`;
+    todoCount.innerHTML = `<strong id="count">${numero}</strong> item${plural} left`;
     guardarLocalStorage()
 }
 
@@ -114,7 +125,7 @@ document.getElementById('new-todo').addEventListener('keyup',(event)=>{
     
     if(event.key==='Enter'){
         var nuevo = {
-            id:topID++,
+            id:topID(),
             title:document.getElementById('new-todo').value.trim(),
             completed:false
         }
@@ -128,16 +139,57 @@ document.getElementById('new-todo').addEventListener('keyup',(event)=>{
 
 document.getElementById('clear-completed').addEventListener('click',(e)=>{
     var ulTodoList = document.getElementById('todo-list').children
-    
     for (let task of Array.from(ulTodoList)) {
         let checkbox = task.getElementsByClassName('toggle')[0];
         if(checkbox.checked) task.remove();
     }
- 
-
+  
     TodoList = TodoList.filter(t => t.completed == false)
+    guardarLocalStorage()
+    var numero = 0
+    TodoList.forEach(t => {
+        if(t.completed==false) numero++
+    })
+    if(numero==TodoList.length){
+        clearCompleted.setAttribute('hidden','hidden')
+    }
+    else{
+        clearCompleted.removeAttribute('hidden')  
+    }
 })
 
+function filtrarTasks(type){
+    var ulTodoList = document.getElementById('todo-list')
+    ulTodoList.innerHTML = ''
+    for(let item of listFilter.children){
+        item.getElementsByTagName('a')[0].className = ''
+    }
+    if(type==='#/'){
+        TodoList.forEach(t => {
+            ulTodoList.appendChild(createTodoItem(t))
+        })
+        listFilter.children[0].children[0].className = 'selected'
+    }else if(type ==='#/pending'){
+        TodoList.filter(t => t.completed == false).forEach(t => {
+            ulTodoList.appendChild(createTodoItem(t))
+        })
+        listFilter.children[1].children[0].className = 'selected'
+    }else if(type ==='#/completed'){
+        TodoList.filter(t => t.completed == true).forEach(t => {
+            ulTodoList.appendChild(createTodoItem(t))
+        })
+        listFilter.children[2].children[0].className = 'selected'
+    }else if(type == ''){
+        TodoList.forEach(t => {
+            ulTodoList.appendChild(createTodoItem(t))
+        })
+        listFilter.children[0].children[0].className = 'selected'
+    }
+}
 
+window.addEventListener('hashchange', function() { 
+    const route = location.hash;
+    filtrarTasks(route)
+});
 
 render();
